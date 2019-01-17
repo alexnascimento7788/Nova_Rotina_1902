e9af2f53323a2978eba479cf389e72539d074c39
Alex Junior Notebook <alexjunior7788@gmail.com>
2019-01-17 17:02:40 -0200

    Atualização Layout

--- Projeto/Tela_Inicial.pas
+++ Projeto/Tela_Inicial.pas
@@ -4,10 +4,19 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Menus, Vcl.StdCtrls, Vcl.ExtCtrls;
 
 type
   TForm1 = class(TForm)
+    lb_1: TLabel;
+    txt_Nfe: TEdit;
+    txt_Nped: TEdit;
+    lb_2: TLabel;
+    lb_3: TLabel;
+    lb_4: TLabel;
+    txt_Usu: TEdit;
+    txt_dt: TEdit;
+    GroupBox1: TGroupBox;
   private
     { Private declarations }
   public
